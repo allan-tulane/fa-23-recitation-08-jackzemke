f6ea2d986bb64f2fc4ddbfa51b6b9f1206a46886
Jack Zemke <95448108+jackzemke@users.noreply.github.com>
2023-12-05 18:00:19 -0600

    done?

--- answers.md
+++ answers.md
@@ -2,8 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Jack Zemke
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
@@ -11,6 +10,7 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 
 - **1b)**
+    - My algorithm is essentially just a modified version of Dijkstra's algorithm, with the only modification being an additional data point in the tuple representing each node in the heap. Therefore, the work and span of my implimentation are the same as the work and span for Dijkstra's algorith, which are $W(n) = O(|E| \log |E|)$ and $S(n) = O(|E| \log |E|)$.
 
 
 
--- main.py
+++ main.py
@@ -2,30 +2,82 @@ from collections import deque
 from heapq import heappush, heappop 
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
-    
+    def help(visited, frontier):
+        # print(frontier)
+        # print()
+        # print('depth:', depth)
+        # print(visited)
+        if len(frontier) == 0:
+            return visited
+        else:
+            distance, node, deep = heappop(frontier)
+            # print(distance,node,deep)
+            # print('visiting', node)
+            if node in visited:
+                # Already visited, so ignore this longer path
+                return help(visited, frontier)
+            else:
+                # We now know the shortest path from source to node.
+                # insert into visited dict.
+                visited[node] = (distance, deep)
+                # depth.append(node)
+                # print('...distance=', distance)
+                # Visit each neighbor of node and insert into heap.
+                # We may add same node more than once, heap
+                # will keep shortest distance prioritized.
+                for neighbor, weight in graph[node]:
+                    # print(neighbor, weight)
+                    heappush(frontier, (distance + weight, neighbor, deep + 1))
+                return help(visited, frontier)
+        
 
+    frontier = []
+    heappush(frontier, (0, source, 0))
+    visited = {}
+    return help(visited, frontier)
     
-    
+graph = {
+                's': {('a', 1), ('c', 4)},
+                'a': {('b', 2)}, # 'a': {'b'},
+                'b': {('c', 8), ('d', 4)}, 
+                'c': {('d', 3)},
+                'd': {},
+                'e': {('d', 0)}
+            }
+
+# result = shortest_shortest_path(graph, 's')
+# print(result)
+
+
 def bfs_path(graph, source):
+    visited = set()
+    queue = deque([(source)])
+    parents = {}
+
+    while queue:
+        node = queue.popleft()
+
+        if node not in visited:
+            # print(parents)
+            # print('visiting',node)
+            visited.add(node)
+            # print(graph[node])
+            # print(visited)
+            for i in graph[node]:
+                if i not in parents.keys():
+                    parents[i] = node
+            try:
+                queue.extend(graph[node] - visited)
+                # print(queue)
+            except:
+                pass
     """
     Returns:
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
     ###TODO
-    pass
+    return {k: parents[k] for k in sorted(parents)}
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -36,13 +88,22 @@ def get_sample_graph():
             }
 
 
+print(bfs_path(get_sample_graph(),'s'))
     
 def get_path(parents, destination):
+    node = destination
+    path = ''
+    while node!= 's':
+        path += node
+        node = parents[node]
     """
     Returns:
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
-    pass
+    return (path[1:] + 's')[::-1]
+
+parents = bfs_path(get_sample_graph(),'s')
+print(get_path(parents, 'd'))
 
